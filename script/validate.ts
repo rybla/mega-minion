0e0d323bc6f32498722aec32d02ec64e63a92843
Henry Blanchette <henry@rybl.net>
2026-01-30 10:48:05 -0500

    validation script

--- script/validate.ts
+++ script/validate.ts
@@ -2,7 +2,10 @@ import { spawn, spawnSync } from "bun";
 
 {
     console.log("Typechecking project...");
-    const typecheckResult = spawnSync(["bun", "run", "typecheck"], { stderr: "inherit", "stdout": "inherit" });
+    const typecheckResult = spawnSync(
+        ["bun", "run", "typecheck"],
+        { stderr: "inherit", "stdout": "inherit" }
+    );
     if (typecheckResult.exitCode !== 0) {
         console.error("Typechecking failed");
         process.exit(1);
@@ -12,7 +15,10 @@ import { spawn, spawnSync } from "bun";
 
 {
     console.log("Linting project...");
-    const lintResult = spawnSync(["bun", "run", "lint"], { stderr: "inherit", "stdout": "inherit" });
+    const lintResult = spawnSync(
+        ["bun", "run", "lint"],
+        { stderr: "inherit", "stdout": "inherit" }
+    );
     if (lintResult.exitCode !== 0) {
         console.error("Linting failed");
         process.exit(1);
@@ -22,16 +28,24 @@ import { spawn, spawnSync } from "bun";
 
 {
     console.log("Starting dev server...");
-    const devServer = spawn(["bun", "run", "dev"], {
-        stderr: "inherit",
-        stdout: "inherit",
-    });
+    const devServer = spawn(
+        ["bun", "run", "dev"],
+        { stderr: "pipe", stdout: "inherit", }
+    );
 
-    // Wait for 1 seconds
-    await Bun.sleep(2000);
-
-    // Terminate the dev server
+    // Wait before killing the dev server
+    await Bun.sleep(1000);
     devServer.kill();
+
+    // check that the dev server exited because of the kill signal "SIGTERM"
+    const stderrOutput = await new Response(devServer.stderr).text();
+    if (devServer.exitCode !== 0 && !stderrOutput.includes("SIGTERM")) {
+        console.log(stderrOutput);
+        console.error("Dev server did not exit because of the kill signal \"SIGTERM\"");
+        process.exit(1);
+    }
 
     console.log("Dev server terminated");
 }
+
+console.log("Validation complete. All checks passed.");
